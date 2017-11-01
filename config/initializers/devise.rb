2d52a22a48503e0244a412feb257574efcb9a10f
Andrew Mulhall <andrewmulhall@Andrews-MacBook-Pro.local>
2017-11-01 18:05:06 +1100

    Added styling

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,6 +261,8 @@ Devise.setup do |config|
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
 
+  config.omniauth :facebook, '327708104368142', 'f16df94b96115a7faea9e554e889858c', scope: 'email', info_fields: 'email, name'
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
